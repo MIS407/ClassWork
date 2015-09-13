8933744f7cbdf17c8a7db43069bdadb5afa80173
DrSree <nilakant@iastate.edu>
2015-09-13 11:38:21 -0500

    Create myCSV1.py

---
+++ myCSV1.py
@@ -0,0 +1,8 @@
+import csv
+
+with open('myData.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        print(row)
+        print(row[0])
+        print(row[0],row[1],row[2],)
