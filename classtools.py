c1052cfb714a061a3051f4454901651ce3ff934d
DrSree <nilakant@iastate.edu>
2015-09-24 23:34:48 -0500

    New class tools file
    General tools class

---
+++ classtools.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 22:28:42 2015
+
+@author: Sree
+"""
+
+#File classtools.py
+"Assorted class utilities and tools"
+
+class AttrDisplay:
+    """
+    Provides an inheritable display overload method that shows instances with
+    their class names and a name=value pair for each attribute stored on the
+    instance itself (but not attrs inherited from its classes). Can be mixed 
+    into any class, and will work on any instance.
+    """
+    def gatherAttrs(self):
+        attrs = []
+        for key in sorted(self.__dict__):
+            attrs.append('%s=%s' % (key, getattr(self, key)))
+            return ', '.join(attrs)
+            
+    def __repr__(self):
+        return '[%s: %s]' % (self.__class__.__name__, self.gatherAttrs())
+        
+if __name__ == '__main__':
+    class TopTest(AttrDisplay):
+        count = 0
+        def __init__(self):
+            self.attr1 = TopTest.count
+            self.attr2 = TopTest.count+1
+            TopTest.count += 2
+            
+    class SubTest(TopTest):
+        pass
+    
+    X, Y = TopTest(), SubTest()
+    
+    print(X)
+    print(Y)
+    
